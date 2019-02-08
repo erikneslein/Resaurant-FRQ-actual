76ad4d4632985b4b4e86419a33866a9f7f827ead
erikneslein <42582469+erikneslein@users.noreply.github.com>
2019-02-08 12:39:09 -0500

    Add files via upload

--- Tester.java
+++ Tester.java
@@ -1,4 +1,6 @@
-
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
 /**
  * Write a description of class Tester here.
  *
@@ -7,27 +9,23 @@
  */
 public class Tester
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class Tester
-     */
-    public Tester()
+    public static void main(String[] args )
     {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+        double[] prices1 = {19.25, 7.50, 8.43, 9.26, 10.72, 3.37, 8.27};
+        String[] foods1 = {"steak", "fish", "chicken", "pork", "shrimp", "crab", "deer"};
+        
+        RestaurantTables eriksBBQ = new RestaurantTables();
+        
+        eriksBBQ.placeTableOrders(0, foods1, prices1);
+        System.out.println("Bill: " + eriksBBQ.totalBill(0));
+        System.out.println("Tip: " + eriksBBQ.computeTip(0));
+        System.out.println();
+        
+        double[] prices2 = {9.38, 6.63, 19.83, 17.37};
+        String[] foods2 = {"steak", "fish", "chicken", "pork"};
+        
+        eriksBBQ.placeTableOrders(1, foods2, prices2);
+        System.out.println("Bill: " + eriksBBQ.totalBill(1));
+        System.out.println("Tip: " + eriksBBQ.computeTip(1));
     }
 }
